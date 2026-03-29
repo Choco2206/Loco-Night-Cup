28d7429ce38bddf16c90048f83f85726e6fda02c
Choco2206 <k.alvaro@gmx.de>
2026-03-29 04:36:32 +0200

    deploy-commands.js aktualisieren

--- deploy-commands.js
+++ deploy-commands.js
@@ -5,7 +5,7 @@ const { REST, Routes, SlashCommandBuilder } = require('discord.js');
 const commands = [
   new SlashCommandBuilder()
     .setName('setup')
-    .setDescription('Erstellt die Rollen-Auswahl für den Server')
+    .setDescription('Erstellt Start-Hier und Rolle-Wählen'),
 ].map(command => command.toJSON());
 
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
