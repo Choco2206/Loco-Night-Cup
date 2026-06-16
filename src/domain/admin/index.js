bcbc999badf74b3f9c6f91019c6e4634595966a7
Choco2206 <k.alvaro@gmx.de>
2026-06-17 00:07:33 +0200

    Route admin select menu interactions

--- src/domain/admin/index.js
+++ src/domain/admin/index.js
@@ -1,15 +1,15 @@
 'use strict';
 
 const { ensureAdminPanel } = require('./admin-panel');
-const { handleAdminButton } = require('./admin-interactions');
+const { handleAdminInteraction } = require('./admin-interactions');
 
 async function init(client) {
   await ensureAdminPanel(client);
 }
 
 async function handleInteraction(interaction, client) {
-  if (!interaction.isButton()) return false;
-  return handleAdminButton(interaction, client);
+  if (!interaction.isButton() && !interaction.isStringSelectMenu()) return false;
+  return handleAdminInteraction(interaction, client);
 }
 
 module.exports = {
