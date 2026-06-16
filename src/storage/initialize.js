e6ed4a8fbba2c148c23014d1b894a28c7a9d9d46
Choco2206 <k.alvaro@gmx.de>
2026-06-16 10:01:03 +0200

    Phase 1: add storage initialization

---
+++ src/storage/initialize.js
@@ -0,0 +1,153 @@
+'use strict';
+
+const { EVENT_KEYS, KNOCKOUT_ROUNDS } = require('../app/constants');
+const {
+  BANS_DIR,
+  DATA_DIR,
+  EVENTS_DIR,
+  FILES,
+  MESSAGES_DIR,
+  SETTINGS_DIR,
+  TEAM_LOGOS_DIR,
+  TEAMS_DIR,
+} = require('./paths');
+const {
+  createBansDefault,
+  createEventDefault,
+  createMessagesDefault,
+  createSettingsDefault,
+  createTeamsDefault,
+} = require('./defaults');
+const { ensureDir, ensureJsonFile, readJson, writeJsonAtomic } = require('./json-store');
+
+function ensureEventKeys(object, factory) {
+  let changed = false;
+
+  for (const eventKey of EVENT_KEYS) {
+    if (!object[eventKey]) {
+      object[eventKey] = factory(eventKey);
+      changed = true;
+    }
+  }
+
+  return changed;
+}
+
+function ensureKnockoutRounds(rounds) {
+  let changed = false;
+
+  for (const roundKey of KNOCKOUT_ROUNDS) {
+    if (!rounds[roundKey]) {
+      rounds[roundKey] = {
+        channelId: null,
+        messageId: null,
+        releaseMessageId: null,
+        reminderMessageIds: [],
+        createdAt: null,
+        updatedAt: null,
+      };
+      changed = true;
+    }
+  }
+
+  return changed;
+}
+
+function normalizeMessagesFile() {
+  const messages = readJson(FILES.messages, createMessagesDefault());
+  let changed = false;
+
+  if (!messages.checkins) {
+    messages.checkins = {};
+    changed = true;
+  }
+
+  if (!messages.groups) {
+    messages.groups = {};
+    changed = true;
+  }
+
+  if (!messages.knockout) {
+    messages.knockout = {};
+    changed = true;
+  }
+
+  if (!messages.ceremony) {
+    messages.ceremony = {};
+    changed = true;
+  }
+
+  changed = ensureEventKeys(messages.checkins, () => ({
+    channelId: null,
+    mainMessageId: null,
+    teamsListMessageIds: [],
+    waitlistMessageIds: [],
+    warningMessageId: null,
+    summaryMessageId: null,
+    createdAt: null,
+    updatedAt: null,
+  })) || changed;
+
+  changed = ensureEventKeys(messages.groups, () => ({
+    cycleKey: null,
+    groups: {},
+  })) || changed;
+
+  changed = ensureEventKeys(messages.knockout, () => ({
+    cycleKey: null,
+    rounds: {},
+  })) || changed;
+
+  changed = ensureEventKeys(messages.ceremony, () => ({
+    cycleKey: null,
+    channelId: null,
+    imageMessageId: null,
+    textMessageId: null,
+    testMessageIds: [],
+    postedAt: null,
+    updatedAt: null,
+  })) || changed;
+
+  for (const eventKey of EVENT_KEYS) {
+    if (!messages.knockout[eventKey].rounds) {
+      messages.knockout[eventKey].rounds = {};
+      changed = true;
+    }
+
+    changed = ensureKnockoutRounds(messages.knockout[eventKey].rounds) || changed;
+  }
+
+  if (changed) {
+    writeJsonAtomic(FILES.messages, messages);
+  }
+
+  return messages;
+}
+
+function initializeStorage() {
+  [
+    DATA_DIR,
+    TEAMS_DIR,
+    TEAM_LOGOS_DIR,
+    EVENTS_DIR,
+    BANS_DIR,
+    MESSAGES_DIR,
+    SETTINGS_DIR,
+  ].forEach(ensureDir);
+
+  ensureJsonFile(FILES.teams, createTeamsDefault);
+  ensureJsonFile(FILES.bans, createBansDefault);
+  ensureJsonFile(FILES.messages, createMessagesDefault);
+  ensureJsonFile(FILES.settings, createSettingsDefault);
+
+  for (const eventKey of EVENT_KEYS) {
+    ensureJsonFile(FILES.events[eventKey], () => createEventDefault(eventKey));
+  }
+
+  normalizeMessagesFile();
+}
+
+module.exports = {
+  initializeStorage,
+  normalizeMessagesFile,
+};
