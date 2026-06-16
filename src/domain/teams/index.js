f0e5435e2368b18eb8d763dd84ee5dbe1be414e6
Choco2206 <k.alvaro@gmx.de>
2026-06-16 11:30:52 +0200

    Phase 2: add team module entrypoint

---
+++ src/domain/teams/index.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const { FILES, readJson } = require('../../storage');
+const { createSettingsDefault } = require('../../storage/defaults');
+const { ensureTeamPanel } = require('./team-panel');
+const { handleInteraction } = require('./team-interactions');
+const { handleMessage } = require('./team-message-handler');
+const { refreshRegisteredTeamsOverview } = require('./team-overview');
+const { handleMemberRemoved } = require('./team-service');
+const { syncAllManagerRoles, syncManagerRoleForUser } = require('./team-roles');
+
+async function init(client) {
+  const settings = readJson(FILES.settings, createSettingsDefault());
+
+  await ensureTeamPanel(client);
+  await refreshRegisteredTeamsOverview(client);
+
+  for (const guild of client.guilds.cache.values()) {
+    await syncAllManagerRoles(guild, settings);
+  }
+}
+
+async function handleGuildMemberRemove(member, client) {
+  if (!member?.id) return false;
+
+  const settings = readJson(FILES.settings, createSettingsDefault());
+  const result = handleMemberRemoved({ userId: member.id });
+  if (!result.changed) return false;
+
+  for (const userId of result.affectedUserIds) {
+    await syncManagerRoleForUser(member.guild, userId, settings).catch(() => {});
+  }
+
+  await refreshRegisteredTeamsOverview(client);
+  return true;
+}
+
+module.exports = {
+  handleGuildMemberRemove,
+  handleInteraction,
+  handleMessage,
+  init,
+};
