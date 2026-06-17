714d308fce36221e2c34d51b5e0589f74972e095
Choco2206 <k.alvaro@gmx.de>
2026-06-17 09:25:18 +0200

    Export group release and cleanup modules

--- src/domain/groups/index.js
+++ src/domain/groups/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const groupReleases = require('./group-releases');
+
 module.exports = {
   ...require('./group-channels'),
   ...require('./group-service'),
@@ -7,7 +9,10 @@ module.exports = {
   ...require('./group-embeds'),
   ...require('./group-interactions'),
   ...require('./group-matches'),
+  ...require('./group-message-cleanup'),
   ...require('./group-posts'),
+  ...groupReleases,
   ...require('./group-results'),
   ...require('./group-roles'),
+  init: groupReleases.initGroupReleases,
 };
