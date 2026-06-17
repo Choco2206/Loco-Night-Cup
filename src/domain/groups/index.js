55e80f10250dfa8f77b88f799002ca8a7b2610ce
Choco2206 <k.alvaro@gmx.de>
2026-06-17 03:35:43 +0200

    Export phase 6 group modules

--- src/domain/groups/index.js
+++ src/domain/groups/index.js
@@ -1,7 +1,12 @@
 'use strict';
 
 module.exports = {
+  ...require('./group-channels'),
   ...require('./group-service'),
   ...require('./group-draw'),
+  ...require('./group-embeds'),
+  ...require('./group-interactions'),
   ...require('./group-matches'),
+  ...require('./group-posts'),
+  ...require('./group-roles'),
 };
