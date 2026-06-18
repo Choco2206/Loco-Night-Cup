9ad1e95e36a5e76c85ae626b5bfa12a646082465
Choco2206 <k.alvaro@gmx.de>
2026-06-18 22:41:09 +0200

    Start check-in reconcile service on boot

--- src/domain/checkins/index.js
+++ src/domain/checkins/index.js
@@ -2,12 +2,15 @@
 
 const { ensureAllCheckinMessages } = require('./checkin-panel');
 const { handleInteraction } = require('./checkin-interactions');
+const { startCheckinReconcile } = require('./checkin-reconcile');
 
 async function init(client) {
   await ensureAllCheckinMessages(client);
+  startCheckinReconcile(client);
 }
 
 module.exports = {
   handleInteraction,
   init,
+  startCheckinReconcile,
 };
