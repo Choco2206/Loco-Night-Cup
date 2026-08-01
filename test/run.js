2aee573bbd5153065029ba0d1d8e9b001a267860
Choco2206 <k.alvaro@gmx.de>
2026-08-01 23:36:32 +0200

    feat: enforce phase timing and KO reminders

--- test/run.js
+++ test/run.js
@@ -4,4 +4,5 @@ require('./team-stream-list.test');
 require('./knockout-release.test');
 require('./group-video-channel.test');
 require('./result-auto-confirm.test');
+require('./phase-deadline.test');
 
