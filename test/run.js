3f1e0fee0f55d8323d37f4a0ee8faf39b81eea19
Choco2206 <k.alvaro@gmx.de>
2026-08-02 09:39:11 +0200

    feat: add Team of the Tournament data flow

--- test/run.js
+++ test/run.js
@@ -4,5 +4,6 @@ require('./team-stream-list.test');
 require('./knockout-release.test');
 require('./group-video-channel.test');
 require('./result-auto-confirm.test');
+require('./team-of-the-tournament.test');
 require('./phase-deadline.test');
 
