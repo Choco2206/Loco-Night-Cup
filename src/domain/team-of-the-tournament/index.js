b186b60b735a8dcd33bcdbce94b550c715e6db11
Choco2206 <k.alvaro@gmx.de>
2026-08-02 09:48:43 +0200

    fix: deploy complete TOTT module

--- src/domain/team-of-the-tournament/index.js
+++ src/domain/team-of-the-tournament/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// Gemeinsamer Einstiegspunkt fuer EA-Club-Verknuepfung und TOTT-Auswertung.
+
 module.exports = {
   ...require('./ea-clubs-client'),
   ...require('./team-of-the-tournament-service'),
