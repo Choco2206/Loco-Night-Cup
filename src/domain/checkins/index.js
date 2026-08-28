6dc8e6b2fd532508bd55eb31d05432bfba5b360c
Choco2206 <k.alvaro@gmx.de>
2026-08-28 11:58:41 +0200

    Initialize Bomber X Loco pre-checkin

--- src/domain/checkins/index.js
+++ src/domain/checkins/index.js
@@ -1,12 +1,19 @@
 'use strict';
 
 const { ensureAllCheckinMessages } = require('./checkin-panel');
-const { handleInteraction } = require('./checkin-interactions');
+const { handleInteraction: handleNormalInteraction } = require('./checkin-interactions');
 const { startCheckinReconcile } = require('./checkin-reconcile');
+const bomberXLocoPrecheckin = require('./bomber-x-loco-precheckin');
 
 async function init(client) {
+  await bomberXLocoPrecheckin.init(client);
   await ensureAllCheckinMessages(client);
   startCheckinReconcile(client);
+}
+
+async function handleInteraction(interaction, client) {
+  if (await bomberXLocoPrecheckin.handleInteraction(interaction, client)) return true;
+  return handleNormalInteraction(interaction, client);
 }
 
 module.exports = {
