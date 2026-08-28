cd3b7d6f7d778fdbbaafe59d73e0b59a362e8361
Choco2206 <k.alvaro@gmx.de>
2026-08-28 12:04:59 +0200

    Initialize Bomber X Loco round of 32 post tracker

--- src/domain/knockout/index.js
+++ src/domain/knockout/index.js
@@ -1,10 +1,24 @@
 'use strict';
 
+const bracket = require('./knockout-bracket');
+const interactions = require('./knockout-interactions');
+const posts = require('./knockout-posts');
+const qualification = require('./knockout-qualification');
+const results = require('./knockout-results');
+const service = require('./knockout-service');
+const { initBomberRound32Posts } = require('./bomber-x-loco-round32-post');
+
+async function initKnockoutReleases(client) {
+  await posts.initKnockoutReleases(client);
+  await initBomberRound32Posts(client);
+}
+
 module.exports = {
-  ...require('./knockout-bracket'),
-  ...require('./knockout-interactions'),
-  ...require('./knockout-posts'),
-  ...require('./knockout-qualification'),
-  ...require('./knockout-results'),
-  ...require('./knockout-service'),
+  ...bracket,
+  ...interactions,
+  ...posts,
+  ...qualification,
+  ...results,
+  ...service,
+  initKnockoutReleases,
 };
