caa05a027ac90c7082a808b1b38ae7c285eb5eb4
Choco2206 <k.alvaro@gmx.de>
2026-09-04 11:48:33 +0200

    Remove Bomber precheckin initialization

--- src/domain/checkins/index.js
+++ src/domain/checkins/index.js
@@ -3,16 +3,13 @@
 const { ensureAllCheckinMessages } = require('./checkin-panel');
 const { handleInteraction: handleNormalInteraction } = require('./checkin-interactions');
 const { startCheckinReconcile } = require('./checkin-reconcile');
-const bomberXLocoPrecheckin = require('./bomber-x-loco-precheckin');
 
 async function init(client) {
-  await bomberXLocoPrecheckin.init(client);
   await ensureAllCheckinMessages(client);
   startCheckinReconcile(client);
 }
 
 async function handleInteraction(interaction, client) {
-  if (await bomberXLocoPrecheckin.handleInteraction(interaction, client)) return true;
   return handleNormalInteraction(interaction, client);
 }
 
